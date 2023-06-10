bd90ce5c1db1c9db5efcecaba7b820739734b767
mathieu <mathieu@fleursdici.fr>
2023-06-10 17:51:43 +0200

    functionnal home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PPDC nouveau site par Mathieu Hourdin';
+export const SITE_DESCRIPTION = 'Bonjour à tous';
+export const HOMEPAGE_URL= 'https://www.ppdc.fr';
